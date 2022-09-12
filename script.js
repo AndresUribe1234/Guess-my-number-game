614cc94dd44f89ea71a94fdecc6b75b04b8cc966
AndresUribe1234 <anduri1997@gmail.com>
2022-09-11 23:52:34 -0500

    first project js interaction

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 'use strict';
-
+/*
 console.log(document.querySelector('nav .nav-left-container').textContent);
 
 // document.querySelector('nav .nav-left-title').textContent =
 //   'first web interaction! 🤖';
 
 console.log(document.querySelector('nav .nav-left-container').textContent);
+
+document.querySelector('.current-score').textContent = 18;
+document.querySelector('.current-input').textContent = 18;
+
+console.log(document.querySelector('.outside-input input').value);
+document.querySelector('.outside-input input').value = 4;
+console.log(document.querySelector('.outside-input input').value);
+*/
+
+document
+  .querySelector('.left-section button')
+  .addEventListener('click', function () {
+    const guess = Number(document.querySelector('.outside-input input').value);
+    console.log(guess);
+    if (!guess) {
+      document.querySelector('.dinamic-message').textContent =
+        'Please enter a valid option! ⚠️';
+    }
+  });
