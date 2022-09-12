fb5caf55cc988f008395e5316519284c440a1f01
AndresUribe1234 <anduri1997@gmail.com>
2022-09-11 22:33:38 -0500

    basic game layout before js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 console.log(document.querySelector('nav .nav-left-container').textContent);
 
-// document.querySelector('nav .nav-left-container').textContent =
+// document.querySelector('nav .nav-left-title').textContent =
 //   'first web interaction! 🤖';
 
 console.log(document.querySelector('nav .nav-left-container').textContent);
